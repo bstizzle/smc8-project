2082012d6f4af6024a74baeea0643ce6f8575578
Ben Stein <whereyouben@gmail.com>
2025-04-10 10:39:26 +0200

    updates

--- OSC_test/osc_client.cpp
+++ OSC_test/osc_client.cpp
@@ -10,14 +10,14 @@ int main() {
     std::srand(std::time(nullptr));
 
     // Create an OSC client that sends to localhost on port 5005
-    lo_address target = lo_address_new("192.168.1.101", "5005");
+    lo_address target = lo_address_new("192.168.1.100", "5005");
 
     if (!target) {
         std::cerr << "Failed to create OSC client!" << std::endl;
         return 1;
     }
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 1000; i++) {
         float random_value = static_cast<float>(std::rand()) / RAND_MAX * 200 + 200;
         lo_send(target, "/randomNum", "f", random_value);  // Send float data
         std::cout << "Sent: " << random_value << std::endl;
